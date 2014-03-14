be47d6feb359dbe7b68b538c8d51c0a38e7e3f7f
cizzit <cizzit@users.noreply.github.com>
2014-03-14 14:41:55 +1100

    Create page4.php

---
+++ visitor/page4.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+     "http://www.w3.org/TR/html4/transitional.dtd">
+<html>
+<head>
+  <meta HTTP-EQUIV=CONTENT-TYPE CONTENT="text/html; charset=utf-8">
+  <title>Slide 4</title>
+</head>
+<body text="#E6E6E6" bgcolor="#003366" link="#ffffff" vlink="#ffffff" alink="#ffffff">
+<center>
+<a href="page3.php"><img src="left.png" border=0 alt="Back"></a> <a href="../process.php"><img src="last.png" border=0 alt="Completed"></a><br /><a href="../process.php">Click here to complete this module</a></center><br>
+<center><img src="img3.png" alt=""></center>
+</body>
+</html>
