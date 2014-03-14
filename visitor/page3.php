6713187abbaaa7414d0727d2766ec04e656dbcbf
cizzit <cizzit@users.noreply.github.com>
2014-03-14 14:41:44 +1100

    Create page3.php

---
+++ visitor/page3.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+     "http://www.w3.org/TR/html4/transitional.dtd">
+<html>
+<head>
+  <meta HTTP-EQUIV=CONTENT-TYPE CONTENT="text/html; charset=utf-8">
+  <title>Slide 3</title>
+</head>
+<body text="#E6E6E6" bgcolor="#003366" link="#0000CC" vlink="#000080" alink="#0000CC">
+<center>
+<a href="page2.php"><img src="left.png" border=0 alt="Back"></a> <a href="page4.php"><img src="right.png" border=0 alt="Continue"></a></center><br>
+<center><img src="img2.png" alt=""></center>
+</body>
+</html>
