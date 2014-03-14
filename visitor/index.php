a633a4d84b370315f8a3d8feb09eec1fd40cfab3
cizzit <cizzit@users.noreply.github.com>
2014-03-14 14:41:20 +1100

    Create index.php

---
+++ visitor/index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+     "http://www.w3.org/TR/html4/transitional.dtd">
+<html>
+<head>
+  <meta HTTP-EQUIV=CONTENT-TYPE CONTENT="text/html; charset=utf-8">
+  <title>Slide 1</title>
+</head>
+<body text="#E6E6E6" bgcolor="#003366" link="#0000CC" vlink="#000080" alink="#0000CC">
+<center>
+<a href="page2.php"><img src="right.png" border=0 alt="Continue"></a></center><br>
+<center><img src="img0.png" alt=""></center>
+</body>
+</html>
